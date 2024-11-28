931b03b84014f800ac58f39c184d6404c46d40d6
Nayonika Roy <nayonikaroy@Jacks-MBP-2.localdomain>
2024-11-27 21:03:04 -0600

    Add point calculation

--- receipt-processor/src/app/api/receipts/process/route.ts
+++ receipt-processor/src/app/api/receipts/process/route.ts
@@ -29,5 +29,34 @@ function calculatePoints(receipt: {
     purchaseTime: string;
     items: { shortDescription: string; price: string }[];
   }): number {
-    return receipt.total.length
+    let points = (receipt.retailer.match(/[a-zA-Z0-9]/g) || []).length;
+
+    if (Number.isInteger(parseFloat(receipt.total))) {
+        points += 50;
+    }
+
+    if ((parseFloat(receipt.total) * 100) % 25 === 0) {
+        points += 25;
+    }
+
+    points += Math.floor(receipt.items.length / 2) * 5;
+
+    receipt.items.forEach(item => {
+        const trimmedDesc = item.shortDescription.trim();
+        if (trimmedDesc.length % 3 === 0) {
+          points += Math.ceil(parseFloat(item.price) * 0.2);
+        }
+    });
+
+    const day = parseInt(receipt.purchaseDate.split('-')[2], 10);
+    if (day % 2 !== 0) {
+        points += 6;
+    }
+
+    const [hour, minute] = receipt.purchaseTime.split(':').map(Number);
+    if (hour === 14 || (hour === 15 && minute < 60)) {
+        points += 10;
+    }
+
+    return points
 }
